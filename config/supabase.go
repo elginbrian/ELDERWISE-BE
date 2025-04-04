cd2f8b5058d4014196f1951b61b79cb0af4533e8
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-04-04 20:55:21 +0700

    fix(twilio): moving env place

--- config/supabase.go
+++ config/supabase.go
@@ -10,8 +10,8 @@ type SupabaseConfig struct {
 
 func NewSupabaseConfig() *SupabaseConfig {
 	return &SupabaseConfig{
-		URL:        os.Getenv("https://ucayizjdmgxgwweshzag.supabase.co"),
-		Key:        os.Getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjYXlpempkbWd4Z3d3ZXNoemFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3NjQwMTgsImV4cCI6MjA1OTM0MDAxOH0.pwFn0khq87rxYRim1lQezFMbot34dSp1xq-8h6XFV0o"),
-		BucketName: os.Getenv("elderwise-images"),
+		URL:        os.Getenv("SUPABASE_URL"),
+		Key:        os.Getenv("SUPABASE_KEY"),
+		BucketName: os.Getenv("SUPABASE_BUCKETNAME"),
 	}
 }
